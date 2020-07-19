69ec976c01367a7de04a9b6f944b1818aa26270d
Daniel Fischer <60206685+dfisch10@users.noreply.github.com>
2020-07-18 19:08:57 -0600

    Add files via upload

---
+++ Assignment9Tests/Assignment9Tests.cs
@@ -0,0 +1,16 @@
+using System;
+using System.Diagnostics.CodeAnalysis;
+using Xunit;
+
+namespace Assignment9Tests.tests
+{
+    [ExcludeFromCodeCoverage]
+    public class Assignment9Tests
+    {
+        [Fact]
+        public void Test1()
+        {
+
+        }
+    }
+}
