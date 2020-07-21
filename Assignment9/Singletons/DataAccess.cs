09005baa362fabc707b3cf376e99316cafd3c478
Daniel Fischer <60206685+dfisch10@users.noreply.github.com>
2020-07-20 19:29:33 -0600

    Add files via upload

--- Assignment9/Singletons/DataAccess.cs
+++ Assignment9/Singletons/DataAccess.cs
@@ -8,20 +8,29 @@ using System.Linq;
 namespace Assignment9
 {
     /// <summary>
-    /// 
+    /// This class is a singleton that accesses the database and handles all methods that interacts with that database, including adding, erasing, updating, and pulling user information from the database for reference/logic.
     /// </summary>
     public sealed class DataAccess
     {
         #region Fields
 
+        /// <summary>
+        /// Initializes a readonly new DataAccess '_instance'
+        /// </summary>
         private static readonly DataAccess _instance = new DataAccess();
 
+        /// <summary>
+        /// Creates a constant name 'FileName' which is the file path for all read/write purposes for the "database".
+        /// </summary>
         private const string FileName = "RockPaperScissorsDB.txt";
 
         #endregion
 
         #region Properties
 
+        /// <summary>
+        /// Assigns the local instance to the class Instance.
+        /// </summary>
         public static DataAccess Instance
         {
             get
@@ -35,7 +44,7 @@ namespace Assignment9
         #region Constructors
 
         /// <summary>
-        /// 
+        /// DataAccess constructor.
         /// </summary>
         private DataAccess()
         {
@@ -46,59 +55,70 @@ namespace Assignment9
         #region Methods
 
         /// <summary>
-        /// 
+        /// This method takes an object of type "IUser" and add it to the database.
         /// </summary>
-        /// <returns></returns>
+        /// <param name="user">The object of type IUser that is attempting to be added to the database</param>
+        public void AddUser(IUser user)
+        {
+            var users = GetUsers();
+            var count = 0;
+
+            if (users is null)
+            {
+                users = new List<User>();
+            }
+
+            for (var index = 0; index < users.Count; index++)
+            {
+                if (users[index]?.Username == user?.Username)
+                {
+                    count++;
+                   // throw new ArgumentException("This username already exists in the database, please select a new username.");        
+                }
+            }
+            if (count == 0)
+            {
+                users?.Add((User)user);
+                var json = JsonConvert.SerializeObject(users);
+                File.WriteAllText(FileName, json);
+            }
+        }
+
+        /// <summary>
+        /// This method creates a new User object and adds it to the database using the AddUser() method.
+        /// </summary>
+        /// <param name="newUser">The new user (username) which is attempting to be created/added to the database</param>
+        /// <returns>returns the user that was created</returns>
+        public IUser CreateNewUser(string newUser)
+        {
+            var user = new User
+            {
+                Username = newUser
+            };
+            DataAccess.Instance.AddUser(user);
+            return user;
+        }
+
+        /// <summary>
+        /// This method checks to see if the designated file exists, and if so, it pulls all the User information from the file and assembles it into a list of type "IUser".
+        /// </summary>
+        /// <returns>returns a list of IUser objects that exist in the database (file)</returns>
         public List<User> GetUsers()
         {
             if (!File.Exists(FileName))
             {
                 File.WriteAllText(FileName, "[]");
             }
-
             var content = File.ReadAllText(FileName);
-
             var users = JsonConvert.DeserializeObject<List<User>>(content);
 
             return users;
-        }
+        }  
 
         /// <summary>
-        /// 
+        /// Updates the scores of the user to add in their new wins/losses/draws to their existing scores after playing.
         /// </summary>
-        /// <param name="user"></param>
-        public void AddUser(IUser user)
-        {
-            var users = GetUsers();
-            var count = 0;
-
-            if(users is null)
-            {
-                users = new List<User>();
-            }
-
-            for(var index = 0; index < users.Count; index++)
-            {
-                if(users[index]?.Username == user?.Username)
-                {
-                    count++;
-                }
-            }
-
-            if (count == 0)
-            {
-                users?.Add((User)user);
-
-                var json = JsonConvert.SerializeObject(users);
-
-                File.WriteAllText(FileName, json);
-            }                
-        }
-
-        /// <summary>
-        /// 
-        /// </summary>
-        /// <param name="user"></param>
+        /// <param name="user">The logged in User object that is currently accessed in the session of the game.</param>
         public void UpdateUser(IUser user)
         {
             var users = GetUsers();
@@ -113,52 +133,36 @@ namespace Assignment9
                 var item = users[index];
                 if(item?.Username == user?.Username)
                 {
+                    //users[index] = user;
                     users[index].Wins += user.Wins;
                     users[index].Losses += user.Losses;
                     users[index].Draws += user.Draws;
                 }
             }
-
             var json = JsonConvert.SerializeObject(users);
-
             File.WriteAllText(FileName, json);
         }
 
         /// <summary>
-        /// 
+        /// Searches through the databases using the GetUsers() method, and then targets and returns the user from that list that contains the username specified.
         /// </summary>
-        /// <param name="username"></param>
-        /// <returns></returns>
+        /// <param name="username">The username of the User object attempting to be retrieved</param>
+        /// <returns>The User object associated with the specifed username</returns>
         public IUser GetSingleUser(string username)
         {
             var users = GetUsers();
-
             return users?.FirstOrDefault(m => m.Username == username);
-        }
+        }      
 
         /// <summary>
-        /// 
-        /// </summary>
-        /// <param name="newUser"></param>
-        /// <returns></returns>
-        public IUser CreateNewUser(string newUser)
-        {
-            var user = new User
-            {
-                Username = newUser
-            };
-
-            DataAccess.Instance.AddUser(user);
-
-            return user;
-        }
-
-        /// <summary>
-        /// 
+        /// This deletes the database file containing all the user's information/scores/etc. 
         /// </summary>
         public void EraseAllUserData()
         {
-            File.Delete(FileName);
+            if (File.Exists(FileName))
+            {
+                File.Delete(FileName);
+            }        
         }
 
         #endregion
--- Assignment9/Singletons/SessionControl.cs
+++ Assignment9/Singletons/SessionControl.cs
@@ -3,14 +3,21 @@
 namespace Assignment9
 {
     /// <summary>
-    /// 
+    /// This class is a singleton that creates a session for a the currently logged in user and handles all methods that interacts with that session, including updating the database and 
+    /// resetting the current session scores after each game mode ends.
     /// </summary>
     public sealed class SessionControl
     {
         #region Fields
 
+        // <summary>
+        /// Initializes a readonly new SessionControl '_session'
+        /// </summary>
         private static readonly SessionControl _session = new SessionControl();
 
+        /// <summary>
+        /// Creates a new User object called "user" which serves as a session instance of the logged in user.
+        /// </summary>
         public User user = new User();
 
         #endregion
@@ -18,7 +25,7 @@ namespace Assignment9
         #region Properties
 
         /// <summary>
-        /// 
+        /// Assigns the local _session to the class Session.
         /// </summary>
         public static SessionControl Session
         {
@@ -33,7 +40,7 @@ namespace Assignment9
         #region Constructor
 
         /// <summary>
-        /// 
+        /// Session Control Constructor
         /// </summary>
         private SessionControl()
         {
@@ -44,7 +51,7 @@ namespace Assignment9
         #region Methods
 
         /// <summary>
-        /// 
+        /// Resets the values for the session users wins, losses, and draws, back to zero. Meant to be used at the conclusion of the various game modes are concluded.
         /// </summary>
         public void ResetSessionScore()
         {
@@ -54,10 +61,10 @@ namespace Assignment9
         }
 
         /// <summary>
-        /// 
+        /// This method updates the database scores for the currently logged in user to reflect the additional wins/losses/etc. from the current session instance, and then resets the current 
+        /// session scores back to zero.
         /// </summary>
-        /// <param name="user"></param>
-        /// <param name="tempScoreHolder"></param>
+        /// <param name="user">The currently logged in user.</param>
         [ExcludeFromCodeCoverage]
         public void UpdateDataBaseAndClearSessionScores(IUser user)
         {
