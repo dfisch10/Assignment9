07c498133603f31806f49d07bbc60445ab352de5
Daniel Fischer <60206685+dfisch10@users.noreply.github.com>
2020-07-20 19:29:50 -0600

    Add files via upload

--- Assignment9/Interfaces/IAiLogic.cs
+++ Assignment9/Interfaces/IAiLogic.cs
@@ -5,7 +5,7 @@ using System.Text;
 namespace Assignment9
 {
     /// <summary>
-    /// 
+    /// Currently unimplemented interface that will contain the AILogic in order to increase the difficulty of the game
     /// </summary>
     interface IAiLogic
     {
--- Assignment9/Interfaces/IUser.cs
+++ Assignment9/Interfaces/IUser.cs
@@ -5,18 +5,33 @@ using System.Text;
 namespace Assignment9
 {
     /// <summary>
-    /// 
+    /// This interface creates an object of type "IUser" which includes properties for that users wins, losses, draws, and winpercentage for whatever game it is implemented in.
     /// </summary>
     public interface IUser
     {
+        /// <summary>
+        /// Gets and Sets the Username of the "User" object to either be created in the database, or loaded from the database.
+        /// </summary>
         string Username { get; set; }
 
+        /// <summary>
+        /// Gets and Sets the amount of "Wins" of the "User" object to either be created in the database, or loaded from the database.
+        /// </summary>
         int Wins { get; set; }
 
+        /// <summary>
+        /// Gets and Sets the amount of "Losses" of the "User" object to either be created in the database, or loaded from the database.
+        /// </summary>
         int Losses { get; set; }
 
+        // <summary>
+        /// Gets and Sets the amount of "Draws" (tied games) of the "User" object to either be created in the database, or loaded from the database.
+        /// </summary>
         int Draws { get; set; }
 
+        /// <summary>
+        /// Gets and Sets the rate of wins the user has based on their wins, losses, and draws, in order to either be created in the database, or loaded from the database.
+        /// </summary>
         int WinPercentage { get; }
     }
 }
